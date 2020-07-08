47b3ee2b173e3bede5276728dd5760829569f193
Selfmade20 <tebogo1686@gmail.com>
2020-07-08 16:08:48 +0200

    Creating form component

---
+++ mcimbi-rsvp/src/container/Bookings.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Bookings extends Component {
+    render() {
+        return (
+            <div className="wrapper">
+                <div className="form-wrapper">
+                    <h1>Create Account</h1>
+                    <form onSubmit={this.handleSubmit} noValidate>
+                        <div className="firstName">
+                            <label htmlFor="firstName">First Name</label>
+                            <input type="text" className="" noValidate placeholder="First Name"/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+
+    }
+}
+
+export default Bookings
