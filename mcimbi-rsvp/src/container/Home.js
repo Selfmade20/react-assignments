c40b010084cd3bfba0a470f8ba297d251fa6dbe2
Selfmade20 <tebogo1686@gmail.com>
2020-07-08 13:42:46 +0200

    Added events details

--- mcimbi-rsvp/src/container/Home.js
+++ mcimbi-rsvp/src/container/Home.js
@@ -5,38 +5,95 @@ import Button from '@material-ui/core/Button';
 class Home extends Component {
     constructor(props) {
         super(props)
-        this.state = []
+        this.state = {
+            isDisplayWorldCup: false,
+            isDisplayCapeTown: false,
+            isDisplayDanceUmbrella: false,
+        }
+    }
+
+    toggleWorldCup = () => {
+        this.setState({
+            isDisplayWorldCup: !this.state.isDisplayWorldCup
+        })
 
     }
 
+    toggleCapeTown = () => {
+        this.setState({
+            isDisplayCapeTown: !this.state.isDisplayCapeTown
+        })
 
-    data = () => {
-        const data = [
-            {
-                name: "fifa world cup"
-            }
-        ]
+    }
+
+    toggleDanceUmbrella = () => {
+        this.setState({
+            isDisplayDanceUmbrella: !this.state.isDisplayDanceUmbrella
+        })
+
     }
 
     render() {
+        const worldCupEvent = [
+            {
+                id: 1,
+                name: "FIFA World Cup",
+                desciption: "The World Cup refers to an international football tournament played between the senior men's national teams from countries that are members of the tournament's governing body, the Fédération Internationale de Football Association(FIFA)",
+                date: "27 July 2022"
+            },
+        ];
+
+        const worldCupResults = worldCupEvent.map(event => {
+            return event.desciption
+        });
+
+        const capeTownEvent = [
+            {
+                id: 2,
+                name: "Cape Town International Jazz Festival",
+                description: "Affectionately referred to as “Africa's Grandest Gathering”, the Cape Town International Jazz Festival (CTIJF) is the largest music event in sub-Saharan Africa. The festival is famous for its star-studded line up of local and international artists."
+            }
+        ];
+        const capeTownResults = capeTownEvent.map(event => {
+            return event.description  
+        });
+
+
+        const danceUmbrella = [
+            {
+                id: 3,
+                name: "",
+                description: "Dance Umbrella is London's international dance festival, celebrating 21st century choreography across the capital. Founded in 1978, Dance Umbrella is internationally recognised for its annual festival which takes place each October."
+            }
+        ];
+        const danceUmbrellaResults = danceUmbrella.map(event => {
+            return event.description
+        });
+
         return (
             <div>
                 <h1 className="header">Active Events</h1>
                 <div className="container">
                     <div className="grid-1">
                         <h3>FIFA World Cup</h3>
-                        <p>The World Cup refers to an international football tournament played between the senior men's national teams from countries that are members of the tournament's governing body, the Fédération Internationale de Football Association (FIFA)</p>
-                        <Button variant="contained" color="primary">Book Seat</Button>
+                        <ul>
+                            {this.state.isDisplayWorldCup ? worldCupResults : null}
+                        </ul>
+                        <Button variant="contained" color="primary" onClick={() => this.toggleWorldCup()}>Show Info</Button>
                     </div>
                     <div className="grid-2">
                         <h3>Cape Town International Jazz Festival</h3>
-                        <p>Affectionately referred to as “Africa's Grandest Gathering”, the Cape Town International Jazz Festival (CTIJF) is the largest music event in sub-Saharan Africa. The festival is famous for its star-studded line up of local and international artists.</p>
-                        <Button variant="contained" color="primary">Book Seat</Button>
+                        <ul>
+                            {this.state.isDisplayCapeTown ? capeTownResults : null}
+                        </ul>
+                        <Button variant="contained" color="primary" onClick={() => this.toggleCapeTown()}>Show Info</Button>
                     </div>
                     <div className="grid-3">
                         <h3>Dance Umbrella</h3>
-                        <p>Dance Umbrella is London's international dance festival, celebrating 21st century choreography across the capital. Founded in 1978, Dance Umbrella is internationally recognised for its annual festival which takes place each October and it aspires to be a hub for choreographic development, programming and debate.</p>
-                        <Button variant="contained" color="primary">Book Seat</Button>
+                        <ul>
+                            {this.state.isDisplayDanceUmbrella ? danceUmbrellaResults : null}
+                        </ul>
+                        <Button variant="contained" color="primary" onClick={() => this.toggleDanceUmbrella()}>Show Info</Button>
                     </div>
                 </div>
             </div>
