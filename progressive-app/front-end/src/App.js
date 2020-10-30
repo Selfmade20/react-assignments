cd9fa06c45c201c1201d766e3912057f6a2494e9
Selfmade20 <tebogo1686@gmail.com>
2020-10-30 12:15:13 +0200

    .

--- progressive-app/front-end/src/App.js
+++ progressive-app/front-end/src/App.js
@@ -1,10 +1,12 @@
-
-import './App.css';
+import "./App.css";
+import JobBoard from "./components/JobBoard";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
     <div className="app">
-     
+      <NavigationBar />
+      <JobBoard />
     </div>
   );
 }
---
+++ progressive-app/front-end/src/components/JobBoard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function JobBoard() {
+  return <div>Hi, I'm a Job Board!</div>;
+}
+
+export default JobBoard;
