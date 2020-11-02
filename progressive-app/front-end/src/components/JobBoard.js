12a765cecf30d32f5ffb0f21b40d5c85aca9a203
Selfmade20 <tebogo1686@gmail.com>
2020-11-02 14:47:13 +0200

    Using props to fetch and render company info

--- progressive-app/front-end/src/components/JobBoard.js
+++ progressive-app/front-end/src/components/JobBoard.js
@@ -1,7 +1,35 @@
 import React from "react";
 
-function JobBoard() {
-  return <div>Hi, I'm a Job Board!</div>;
+function JobBoard({ job }) {
+  const langAndTools = [];
+
+  if (job.languages) {
+    langAndTools.push(...job.languages);
+  }
+  if (job.tools) {
+    langAndTools.push(...job.tools);
+  }
+  return (
+    <div className="flex bg-white shadow-md my-4 p-6 ml-4">
+      <div>{/* <img src={job.logo} alt={job.company} /> */}</div>
+      <div className="flex flex-col justify-between ml-4">
+        <h3 className="font-bold text-teal-500">{job.company}</h3>
+        <h2 className="font-bold text-xl">{job.position}</h2>
+        <p className="text-gray-600">
+          {job.postedAt} · {job.contract} · {job.location}
+        </p>
+      </div>
+      <div className="flex items-center ml-auto">
+        {langAndTools
+          ? langAndTools.map((langAndTool) => (
+              <span className="text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded">
+                {langAndTool}
+              </span>
+            ))
+          : ""}
+      </div>
+    </div>
+  );
 }
 
 export default JobBoard;
