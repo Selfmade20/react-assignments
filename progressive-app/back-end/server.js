4defee7086904091fabefc9c67ed6610e9d347e9
Selfmade20 <tebogo1686@gmail.com>
2020-11-05 13:56:43 +0200

    Configuring express server

--- progressive-app/back-end/server.js
+++ progressive-app/back-end/server.js
@@ -3,28 +3,8 @@ const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
 const port = process.env.PORT || 4000;
-const merchant_model = require('./model/merchant_model')
 dotenv.config();
 
-app.use(express.json());
 app.use(cors());
-app.use(function(req, res, next){
-    res.sendHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-    next();
-})
-
-
-app.get('/', (req, res) => {
-    merchant_model.getUsers()
-    .then(response => {
-        res.status(200).send(response)
-    })
-    .catch(error => {
-        res.status(500).send(error);
-    })
-})
-
-
+app.use(express.json());
 app.listen(port, console.log(`Server successfully running on port: ${port}`));
